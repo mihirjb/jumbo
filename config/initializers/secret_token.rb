f0f5edc96e7b5c6cdb1147a39eaf6f194ced51b1
Mihir <mihir@5ve.in>
2013-03-14 14:38:03 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jumbo::Application.config.secret_token = 'c96dde68c7f32059086fce1041c686f79e8451e6794f937beaa2365761d15de4c8a5417fc5fe65f775c068d5545fcec93f970a34299aa2029dd099ed5fa4cac5'
---
+++ db/migrate/20130314090711_devise_create_travelagents.rb
@@ -0,0 +1,28 @@
+class DeviseCreateTravelagents < ActiveRecord::Migration
+  def self.up
+    create_table(:travelagents) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+
+      # t.encryptable
+      # t.confirmable
+      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
+      # t.token_authenticatable
+
+
+      t.timestamps
+    end
+
+    add_index :travelagents, :email,                :unique => true
+    add_index :travelagents, :reset_password_token, :unique => true
+    # add_index :travelagents, :confirmation_token,   :unique => true
+    # add_index :travelagents, :unlock_token,         :unique => true
+    # add_index :travelagents, :authentication_token, :unique => true
+  end
+
+  def self.down
+    drop_table :travelagents
+  end
+end
